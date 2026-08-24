ad4ee1629adee252089e7999c35e20a6116e7ccc
JordanGoh4 <lowonmath@gmail.com>
2026-08-24 14:46:45 +0800

    Update name

---
+++ src/main/java/Luck.java
@@ -0,0 +1,10 @@
+public class Luck {
+    public static void main(String[] args) {
+        String banner = " _                _        \n"
+                + "| |    _   _  ___| | __    \n"
+                + "| |   | | | |/ __| |/ /    \n"
+                + "| |___| |_| | (__|   <     \n"
+                + "|_____|\\__,_|\\___|_|\\_\\    \n";
+        System.out.println(banner);
+    }
+}
